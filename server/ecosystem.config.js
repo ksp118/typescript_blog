223c0f6c152e5e0781312ec9b0b3bfdeb5a67e3a
Gyuho Lee <ece@uos.ac.kr>
2025-07-05 19:55:08 +0900

    fix:yet another another another workflow update

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -2,17 +2,26 @@ module.exports = {
   apps: [
     {
       name: "blog-backend",
-      script: "../dist/server.js", // 트랜스파일된 JS 엔트리 포인트
-      cwd: ".", // 현재 디렉토리 기준
-      watch: true,
-      ignore_watch: ["node_modules"],
+      script: "/srv/blog-backend/dist/server.js", // 트랜스파일된 JS 엔트리 포인트
+      cwd: "/srv/blog-backend", // 현재 디렉토리 기준
+
+      watch: false,
       exec_mode: "cluster", // 여러 CPU 코어 활용
       instances: "max", // 가능한 모든 코어 사용
+
+      max_memory_restart: "300M",
+      listen_timeout: 8000,
+      graceful_timeout: 8000,
+
+      ignore_watch: ["node_modules", "logs"],
+
       env: {
         NODE_ENV: "production",
       },
-      output: "/home/ksp118/logs/pm2/console.log", // 로그 경로 (절대경로로 수정)
-      error: "/home/ksp118/logs/pm2/consoleError.log",
+
+      output: "/srv/blog-backend/logs/pm2/console.log", // 로그 경로 (절대경로로 수정)
+      error: "/srv/blog-backend/logs/pm2/consoleError.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
     },
   ],
 };
